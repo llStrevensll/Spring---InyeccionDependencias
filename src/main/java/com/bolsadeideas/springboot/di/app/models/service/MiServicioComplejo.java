1d0ac5f319cbba6e1c905787c5690f7ad1928f4d
Steven <anguelr12@hotmail.com>
2020-03-21 21:00:52 -0500

    Inyeccion Dependencias

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.di.app.models.service;
+
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.stereotype.Component;
+
+//@Component("miServicioComplejo")
+public class MiServicioComplejo implements IServicio{
+	
+	@Override
+	public String operacion() {
+		return "ejecutando operacion complicada...";
+	}
+}
